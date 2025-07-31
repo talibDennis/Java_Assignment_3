785ceaddab668067f70df812d93184176042145d
talibDennis <joseph.dennis@triosstudent.com>
2025-07-31 18:34:05 -0400

    Added Validator.java

---
+++ utility/Validator.java
@@ -0,0 +1,61 @@
+package utility;
+
+import java.util.Scanner;
+
+public class Validator {
+  Scanner input = new Scanner(System.in);
+  int choice = 0;
+
+  // constructor
+  public Validator() {} // zero-arg constructor
+
+  public int validatePick(int maxVal) {
+    choice = 0;
+
+    while (true) {
+      System.out.printf("Enter a number between 1 & %d:%n", maxVal);
+      try { // wraps code that can break
+        choice = input.nextInt();
+
+        if (choice >= 1 && choice <= maxVal) {
+          System.out.println("You entered: " + choice);
+          break; // valid input, exit loop
+        } else {
+          System.out.printf("Invalid number. Please enter a number between 1 & %d%n", + maxVal);
+        }
+      } // try
+      catch (NumberFormatException e) {
+        System.out.println("That's not a valid number. Please try again.");
+      } // catch
+      finally { // always runs!
+        // do nothing
+      }
+    } // while
+
+    return choice;
+  } // validatePick()
+
+  public int validateAttackPick(int maxVal) {
+    choice = 0;
+
+    while (true) {
+      System.out.printf("Enter a number between 1 & %d:$n", maxVal);
+      try {
+        choice = input.nextInt();
+
+        if (choice >= 1 && choice <= maxVal) {
+          System.out.println("You entered: " + choice);
+          break; // valid input, exit loop
+        } else {
+          System.out.printf("Invalid number. Please enter from 1 to %d%n", maxVal);
+        }
+      } // try
+      catch (NumberFormatException e) {
+        System.out.println("That's not a valid number. Please try again.");
+      } // catch
+    } // while
+
+    return choice;
+  } // validateAttackPick()
+
+} // class
