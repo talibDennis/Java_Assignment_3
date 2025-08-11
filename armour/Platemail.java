49ae6ba543eaf802ba28b5c911f62ed60233dc39
talibDennis <joseph.dennis@triosstudent.com>
2025-08-10 22:58:07 -0400

    Updated Platemail.java code

--- armour/Platemail.java
+++ armour/Platemail.java
@@ -2,20 +2,20 @@ package armour;
 
 public class Platemail extends Armour {
   // should be a calc with damage NOT a straight protection amount
-  private int armourAmount = 125; // 75 - 125 = -50 no damage!
-  private int dexCost = 50;
+  private int protectionAmount = 60; // calc a damage of 60%
+  private int dexCost = 65;
   private int armourDurabilityFactor = 15;
 
   public Platemail() {
     super("Platemail");
-    super.setArmourAmount(armourAmount);
+    super.setProtectionAmount(protectionAmount);
     super.setDexCost(dexCost);
   }
 
   // setters
   public void setArmourReduction(int damageAmount) {
     // 10 damage would take away 1 armour points
-    this.armourAmount -= damageAmount / armourDurabilityFactor;
+    this.protectionAmount -= damageAmount / armourDurabilityFactor;
   }
 
 } // class
