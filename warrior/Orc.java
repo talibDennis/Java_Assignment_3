ae60dddeed2c500c9aa9410db082da2c6d12c508
talibDennis <joseph.dennis@triosstudent.com>
2025-08-10 21:17:13 -0400

    Updated Orc.java code

--- warrior/Orc.java
+++ warrior/Orc.java
@@ -1,19 +1,19 @@
 package warrior;
 
 public class Orc extends Warrior {
-    private int minHealth = 180; // min amount a Orc warrior starts with
-    private int healthRange = 100; // the bonus or extra range for the health stat
-    private int minStrength = 150;
-    private int strengthRange = 80;
+    private int minHealth = 350; // min amount a Orc warrior starts with
+    private int bonusHealth = 90; // the bonus or extra range for the health stat
+    private int minStrength = 120;
+    private int bonusStrength = 70;
     private int minDexterity = 20;
-    private int dexterityRange = 30;
+    private int bonusDexterity = 10;
 
     public Orc() {
         super("Orc");
         // 0 - 99 + 180 = 180 to 279
-        super.setHealth(super.randNum.nextInt(this.healthRange) + this.minHealth);
-        super.setStrength(super.randNum.nextInt(this.strengthRange) + this.minStrength);
-        super.setDexterity(super.randNum.nextInt(this.dexterityRange) + this.minDexterity);
+        super.setHealth(super.randNum.nextInt(this.bonusHealth) + this.minHealth);
+        super.setStrength(super.randNum.nextInt(this.bonusStrength) + this.minStrength);
+        super.setDexterity(super.randNum.nextInt(this.bonusDexterity) + this.minDexterity);
     }
 
     // getters
